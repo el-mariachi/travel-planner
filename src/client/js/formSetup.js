0588cb9cdb5f3d7a3275c68b512b487deb768667
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-21 09:28:22 +0300

    fix: Change formSetup import dependencies directly
    In production build the document load event didn't triger the
    listener added in a module. Removing that listener and importing
    a dependency directly instead of using Client library fixed the issue.

--- src/client/js/formSetup.js
+++ src/client/js/formSetup.js
@@ -1,12 +1,10 @@
+import { newTripSubmitHandler } from './newtrip';
 import { locationAutoFill } from './locationAutoFill';
 
 const formSetup = () => {
     const form = document.forms.newtrip;
-    console.log(form);
-    document.addEventListener('load', () => {
-        form.addEventListener('submit', Client.newTripSubmitHandler);
-        form.elements.destination.addEventListener('keyup', locationAutoFill);
-    });
+    form.addEventListener('submit', newTripSubmitHandler);
+    form.elements.destination.addEventListener('keyup', locationAutoFill);
 };
 
 export { formSetup };
