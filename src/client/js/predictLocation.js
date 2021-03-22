33f12004b09e1986b4e44efb8df91d8937252ae9
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-22 17:33:54 +0300

    chore: Add comments to predictLocation fn

--- src/client/js/predictLocation.js
+++ src/client/js/predictLocation.js
@@ -6,6 +6,8 @@ import { debounceAsync } from './debounceAsync';
 const debouncedLocation = debounceAsync(getLocations, 500);
 
 const destRegEx = /^[\w, ]{2,}$/;
+
+// destination form input handler
 // using regular function declaration here in order
 // to be able to use 'this' inside the function
 export function predictLocation(event) {
@@ -14,16 +16,20 @@ export function predictLocation(event) {
     if (!destRegEx.test(this.value)) {
         return;
     }
-    // found locations list div
+    // get element to put predictions into
     const locations = this.closest('.newtrip__section').querySelector('.locations__inner');
+    // prep string for fetch
     const query = encodeURIComponent(this.value);
+    // make a call to backend. fill list upon resolve
+    // in the meantime display spinner
     debouncedLocation(query)
         .then(response => {
+            // if nothing is returned, do nothing
             if (!Array.isArray(response) || response.length === 0) {
                 hideLocations();
                 return;
             }
-            // render list
+            // otherwise render list of predictions
             locations.innerHTML = suggestionsHTML(response);
             locations.parentElement.classList.add('locations--visible');
         })
