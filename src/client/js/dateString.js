3c5363e1ff14abbf6f02bcd6b0584837a67364b0
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-25 20:20:46 +0300

    refactor: Use toISOString() in dateString function

--- src/client/js/dateString.js
+++ src/client/js/dateString.js
@@ -2,10 +2,11 @@ const dateString = date => {
     if (!(date instanceof Date)) {
         return date;
     }
-    const month = date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
-    const day = date.getDate() >= 10 ? date.getDate() : '0' + date.getDate();
+    return date.toISOString().substr(0, 10);
+    // const month = date.getMonth() >= 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+    // const day = date.getDate() >= 10 ? date.getDate() : '0' + date.getDate();
 
-    return `${date.getFullYear()}-${month}-${day}`;
+    // return `${date.getFullYear()}-${month}-${day}`;
 };
 
 export { dateString };
