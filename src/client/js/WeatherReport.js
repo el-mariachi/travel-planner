eb162f33ef6e02f05b2dce791a22050a0ce82117
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-26 13:56:33 +0300

    fix: Wrap report in div

---
+++ src/client/js/WeatherReport.js
@@ -0,0 +1,8 @@
+import { Component } from './Component';
+import Mustache from 'mustache';
+
+export class WeatherReport extends Component {
+    constructor(el, props) {
+        super(el, props);
+    }
+}
