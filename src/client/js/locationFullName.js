389eb22b621153e6ead4edeb1826669b510c93bd
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-26 10:31:53 +0300

    refactor: Change locationFullName: use Array.join

--- src/client/js/locationFullName.js
+++ src/client/js/locationFullName.js
@@ -1,7 +1,6 @@
 // returns 'City, State(or else), Country'
 const locationFullName = geoname => {
-    const picked = (({ name, countryName, adminName1 }) => ({ name, countryName, adminName1 }))(geoname);
-    return `${picked.name}, ${picked.adminName1}, ${picked.countryName}`;
+    return (({ name, countryName, adminName1 }) => ([name, adminName1, countryName].join(', ')))(geoname);
 }
 
 export { locationFullName };
