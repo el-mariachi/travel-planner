ed6b3787f25740a7b10f75bb3fa5f4dc92ed553c
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-03-27 17:56:45 +0300

    chore: Change settings for heroku

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@
 // no need to listen to the port fot the tests
 const { app } = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // start server
 app.listen(port, () => {
