1b004d19a0f0580d5a100b03b9cedd0dc62bfe9c
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-04-08 15:48:21 +0300

    test: Test TripHead

---
+++ __test__/TripHead.test.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+import { TripHead } from "../src/client/js/TripHead";
+import Mustache from "mustache";
+import { tripHeadTemplate } from '../src/client/views/tripHead.tmpl';
+
+const mockRender = jest.fn();
+const mustacheSpy = jest.spyOn(Mustache, 'render');
+
+const headData = {
+    countdown: 3,
+    locationFullName: 'London',
+    from: '2000-11-11',
+    to: '2000-12-12'
+}
+
+describe('Testing TripHead functionality', () => {
+    const div = document.createElement('div');
+    const spyRender = jest.spyOn(TripHead.prototype, 'render');
+    const head = new TripHead(div);
+    it('should not render initially', () => {
+        expect(spyRender).toHaveBeenCalledTimes(0);
+        spyRender.mockRestore();
+    });
+    it('should render template with correct data', () => {
+        head.setProps(headData);
+        expect(mustacheSpy).toHaveBeenCalledWith(tripHeadTemplate, {
+            countdown: 3,
+            name: 'London',
+            from: '11/11/2000',
+            to: '12/12/2000'
+        });
+    });
+});
