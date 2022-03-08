6f510b002ff26c1065f843c2651b150fb243fea4
Dmitri Sokolov <mitson.mbox@gmail.com>
2022-03-08 22:01:00 +0300

    test: Add testing-library-dom

--- src/client/js/__mocks__/suggestionsFragment.ts
+++ src/client/js/__mocks__/suggestionsFragment.ts
@@ -1,7 +1,5 @@
 export const suggestionsFragment = jest.fn(() => {
     const li = document.createElement('li');
     li.textContent = 'List Item';
-    const fragment = new DocumentFragment();
-    fragment.appendChild(li);
-    return fragment;
+    return li;
 });
